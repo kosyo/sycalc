b7a3ac6f61a7375c53faeb2d04a4cfbcdb1d9c21
kosyo <kosyokk@gmail.com>
2016-05-31 02:45:53 +0300

    initial commit

---
+++ calc_client.pl
@@ -0,0 +1,28 @@
+use strict;
+use Socket;
+use Data::Dumper;
+my $host = "localhost";
+my $port = 5000;
+
+socket(F, PF_INET, SOCK_STREAM, getprotobyname('tcp')) || die $!;
+my $internet_addr = inet_aton($host)
+     or die "Couldn't convert  into an Internet address: $!\n";
+my $sin = sockaddr_in($port, $internet_addr);
+
+connect(F, $sin)
+     or die "Couldn't connect to $host:$port: $!\n";
+  
+   my $old_fh = select(F); 
+     $| = 1;                # don't buffer output
+       select($old_fh);
+my $line = <STDIN>;
+ 
+my @arr = $line =~ m/\d+|\+|\*\*|\*|\/|-/g; 
+my $expr = join(",", @arr) . "\n";
+syswrite F, $expr, length $expr;
+my $l = <F>;
+print $l;
+
+close(F);
+
+
---
+++ calc_server.pl
@@ -0,0 +1,109 @@
+use strict;
+use Socket;
+use Storable;
+use Data::Dumper;
+use Fcntl;
+my $operator_precedence_table = {
+        '+' => {p => 1, a => 'l'},
+        '-' => {p => 1, a => 'l'},
+        '*' => {p => 2, a => 'l'},
+        '/' => {p => 2, a => 'l'},
+        '**' => {p => 3, a => 'r'},
+};
+
+sub ShuntingYardCalc(@)
+{
+    my (@arr) =  @_;
+    my $i = 0;
+    my $next = 0;
+    for my $el (@arr)
+    {
+        if(($i != 0 || $el !~ m/^\d+$/) && ($el !~ m/^\d+$/ && !defined $$operator_precedence_table{$el} 
+                || (defined $$operator_precedence_table{$el} && $arr[$i - 1] !~ m/^\d+$/)
+                || ($el =~ m/^\d+$/ && $arr[$i - 1] =~ m/^\d+$/)))
+        {
+            my $msg = "Invalid input\n";
+            syswrite FH, $msg, length $msg;
+            $next = 1;
+            last;
+        }
+        $i++;
+
+    }
+    if($next)
+    {
+        next;
+    }
+
+    my (@nums, @ops, $sum);
+    my $j = 0;
+    while(@arr)
+    {   
+        push @nums, pop @arr;
+        if($j != 0 && ($$operator_precedence_table{$arr[ @arr - 1]}{p} < $$operator_precedence_table{$ops[@ops - 1]}{p} ))
+        {
+            do
+            {
+                my $num1 = pop @nums;
+                my $num2 = pop @nums;
+                my $op = pop @ops;
+                $sum = eval("$num1 $op $num2");
+                print "sum is: $sum";
+                push @nums, $sum;
+            } while($$operator_precedence_table{$ops[@ops - 1]}{a} eq 'r');
+        }
+        if(@arr)
+        {
+            push @ops, pop @arr;
+        }
+        $j++;
+    }   
+    print Dumper @nums;
+    print Dumper @ops;
+
+    my $res;
+    while(@nums > 1)
+    {
+        my $num1 = pop @nums;
+        my $num2 = pop @nums;
+        my $op = pop @ops;
+
+        print("izprazvane na opashkata, $num1, $num2, $op");
+        $res = eval("$num1 $op $num2");
+        push @nums, $res;
+    }
+
+    return $nums[0];
+}
+
+socket(F, PF_INET, SOCK_STREAM, getprotobyname('tcp')) || die $!;
+my $sin = sockaddr_in (5000, INADDR_ANY);
+my $length = 10;
+bind(F,$sin)  || die $!;
+listen(F, $length) || die $!;
+while(accept(FH, F))
+{
+    next if my $pid = fork;
+    die "fork: $!" unless defined $pid;
+    close(F);
+    print("[$$] CONNECTED\n");
+
+    while(<FH>)
+    {
+        print("[$$] $_\n");
+        my @arr = split(/,/, $_);
+        
+        my $res = ShuntingYardCalc(@arr);
+        $res .= "\n";  
+            
+        syswrite FH, $res, length $res ;
+        sysopen (RES, "result.txt", O_WRONLY|O_CREAT|O_APPEND, 0755) 
+            or die "cannot be opened. $!";
+        syswrite RES, $res, length $res;
+        close RES;
+
+    }
+    close(FH);
+    print("[$$] EXIT\n");
+    exit;
+}
