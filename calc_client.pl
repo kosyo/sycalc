47d9122998b3fd5b7521a606e9cd21e533810829
kosyo <kosyokk@gmail.com>
2016-05-31 03:02:12 +0300

    ParseExpression function is added

--- calc_client.pl
+++ calc_client.pl
@@ -1,28 +1,40 @@
 use strict;
 use Socket;
 use Data::Dumper;
-my $host = "localhost";
-my $port = 5000;
 
-socket(F, PF_INET, SOCK_STREAM, getprotobyname('tcp')) || die $!;
-my $internet_addr = inet_aton($host)
-     or die "Couldn't convert  into an Internet address: $!\n";
-my $sin = sockaddr_in($port, $internet_addr);
+sub ParseExpression($)
+{
+    my($line) = @_;
+    my @arr = $line =~ m/\d+|\+|\*\*|\*|\/|-/g;
+    my $expr = join(",", @arr) . "\n";
+    return $expr;
+}
 
-connect(F, $sin)
-     or die "Couldn't connect to $host:$port: $!\n";
-  
-   my $old_fh = select(F); 
-     $| = 1;                # don't buffer output
-       select($old_fh);
-my $line = <STDIN>;
- 
-my @arr = $line =~ m/\d+|\+|\*\*|\*|\/|-/g; 
-my $expr = join(",", @arr) . "\n";
-syswrite F, $expr, length $expr;
-my $l = <F>;
-print $l;
+sub StartClient()
+{
+    my $host = "localhost";
+    my $port = 5000;
 
-close(F);
+    socket(F, PF_INET, SOCK_STREAM, getprotobyname('tcp')) || die $!;
+    my $internet_addr = inet_aton($host)
+        or die "Couldn't convert  into an Internet address: $!\n";
+    my $sin = sockaddr_in($port, $internet_addr);
 
+    connect(F, $sin)
+        or die "Couldn't connect to $host:$port: $!\n";
 
+    my $old_fh = select(F); 
+    $| = 1;                # don't buffer output
+    select($old_fh);
+    my $line = <STDIN>;
+
+    my $expr = ParseExpression($line);
+    syswrite F, $expr, length $expr;
+    my $l = <F>;
+    print $l;
+
+    close(F);
+
+}
+
+StartClient();
